e3f95a257da3f36d1693e8c594d86fc6e4d7ed9c
That_Guy <eghove@gmail.com>
2019-07-13 17:12:16 -0500

    finishing first round of services

--- recipe-app/src/app/recipe/recipe.service.ts
+++ recipe-app/src/app/recipe/recipe.service.ts
@@ -1,15 +1,29 @@
 import { Recipe } from "./recipe.model";
-import { EventEmitter } from "@angular/core";
+import { EventEmitter, Injectable } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
+import { ShoppingListService } from "../shopping-list/shopping-list.service";
 
+@Injectable()
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe ('Test Recipe', 'this is a test', 'https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940')
+    new Recipe (
+      'Test Recipe', 
+      'this is a test', 
+      'https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+      [new Ingredient('Meat', 6),
+      new Ingredient('Cabbage', 4)])
   ];
+
+  constructor(private shoppingListService: ShoppingListService) {}
 
   // return a copy of the recipes array
   getRecipes() {
     return this.recipes.slice();
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.shoppingListService.addIngredients(ingredients);
   }
 }
